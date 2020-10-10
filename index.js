936b26cfeda47f0305f4a4e3f00beab3632ec424
KaladaJumbo <wkalada1@gmail.com>
2020-10-10 19:24:04 -0400

    final commit

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let hours = timeString.split(":");
+  //let mins = parseInt(hours[1]);
+  hours = parseInt(hours[0]);
+  if( hours < 12  ){
+    return "Good Morning"
+  }
+  else if(hours >= 12 && hours < 17){
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  greeting = document.querySelector("#greeting")
+  greeting.innerText = string
+}
